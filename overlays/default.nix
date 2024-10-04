ea0f403d0aae4e8356b2c1217577f8082eb800a0
chillcicada <2210227279@qq.com>
2024-10-04 22:51:59 +0800

    feat: add overlays

---
+++ overlays/default.nix
@@ -0,0 +1,19 @@
+{ inputs
+, ...
+}:
+
+{
+  # This one brings our custom packages from the 'pkgs' directory
+  additions = final: _prev: import ../pkgs {pkgs = final;};
+
+  # This one contains whatever you want to overlay
+  modifications = final: prev: {
+  };
+
+  master-packages = final: _prev: {
+    master = import inputs.nixpkgs-master {
+      system = final.system;
+      config.allowUnfree = true;
+    };
+  };
+}
