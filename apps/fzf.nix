1de614aff3d6c01a7462aa21720e6a516c22f09c
chillcicada <2210227279@qq.com>
2024-10-23 21:46:51 +0800

    chore: modify structure

--- apps/fzf.nix
+++ apps/fzf.nix
@@ -8,5 +8,22 @@
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+
+    # https://github.com/catppuccin/fzf
+    # catppuccin-mocha
+    colors = {
+      "bg+" = "#313244";
+      "bg" = "#1e1e2e";
+      "spinner" = "#f5e0dc";
+      "hl" = "#f38ba8";
+      "fg" = "#cdd6f4";
+      "header" = "#f38ba8";
+      "info" = "#cba6f7";
+      "pointer" = "#f5e0dc";
+      "marker" = "#f5e0dc";
+      "fg+" = "#cdd6f4";
+      "prompt" = "#cba6f7";
+      "hl+" = "#f38ba8";
+    };
   };
 }
