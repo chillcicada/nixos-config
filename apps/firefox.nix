edf3502ff11d429e6bd96909cbf8da6c3cb8db84
chillcicada <2210227279@qq.com>
2024-11-15 11:52:02 +0800

    chore: firefox

--- apps/firefox.nix
+++ apps/firefox.nix
@@ -7,14 +7,5 @@
 {
   programs.firefox = {
     enable = true;
-
-    profiles.default = {
-      id = 0;
-      isDefault = true;
-
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-
-      ];
-    };
   };
 }
