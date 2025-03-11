8c0723bb58689aa1b5e10993c8870a2780bde839
chillcicada <2210227279@qq.com>
2025-03-11 23:26:10 +0800

    chore: modify nix config, prune unused apps

--- core/nix.nix
+++ core/nix.nix
@@ -1,10 +1,17 @@
-{ vars
+{ pkgs
+, lib
+, vars
+, inputs
 , ...
 }:
 
 {
   nix = {
+    package = pkgs.nix;
+    channel.enable = false;
+    registry.nixpkgs.flake = inputs.nixpkgs;
     settings = {
+      nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
       auto-optimise-store = true;
       experimental-features = [ "nix-command" "flakes" ];
       use-xdg-base-directories = true;
@@ -17,19 +24,17 @@
       ];
       trusted-public-keys = [
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "chillcicada.cachix.org-1:nW+OhwrpD0z5oGcBFt/aLEtzN94XcJBm81/cAHsEwVU="
       ];
     };
 
-    channel = {
-      enable = false;
-    };
-
-    # Storage optimization
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 7d";
     };
   };
+
+  environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
 }
