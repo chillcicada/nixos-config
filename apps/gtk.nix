832649749929a4c0af831871411c1aa9afa42850
chillcicada <2210227279@qq.com>
2025-03-12 10:31:27 +0800

    feat: update gtk config and env for fcitx5 in gnome

--- apps/gtk.nix
+++ apps/gtk.nix
@@ -12,7 +12,15 @@
       name = "Papirus-Dark";
     };
 
+    gtk2.extraConfig = ''
+      gtk-im-module = "fcitx"
+    '';
+
     gtk3.extraConfig = {
+      gtk-im-module = "fcitx";
+    };
+
+    gtk4.extraConfig = {
       gtk-im-module = "fcitx";
     };
   };
