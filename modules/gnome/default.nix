6bfd937c51cab88b659e11eb96a5150a5bd65427
chillcicada <2210227279@qq.com>
2025-04-16 17:20:49 +0800

    feat: remove more gnome software

--- modules/gnome/default.nix
+++ modules/gnome/default.nix
@@ -1,29 +1,71 @@
-{ pkgs, vars, ... }:
+{
+  pkgs,
+  vars,
+  lib,
+  ...
+}:
 
 {
   # X11 & GNOME
-  services.xserver.enable = true;
-  services.xserver.xkb.layout = "us";
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      excludePackages = with pkgs; [ xterm ];
 
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.excludePackages = with pkgs; [ xterm ];
+      xkb = {
+        layout = "us";
+      };
+    };
 
-  services.gnome.core-utilities.enable = true;
+    gnome = {
+      core-utilities.enable = true;
+
+      games.enable = false;
+      gnome-user-share.enable = false;
+      gnome-initial-setup.enable = false;
+      gnome-remote-desktop.enable = false;
+      gnome-online-accounts.enable = false;
+      gnome-browser-connector.enable = false;
+      evolution-data-server.enable = lib.mkForce false;
+    };
+
+    # Enable automatic login for the user.
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = vars.userName;
+      };
+    };
+
+    udev.packages = with pkgs; [ gnome-settings-daemon ];
+  };
+
   environment.gnome.excludePackages = with pkgs; [
     gnome-logs
     gnome-maps
     gnome-tour
     gnome-music
+    gnome-clocks
+    gnome-console
+    gnome-weather
+    gnome-calendar
     gnome-contacts
+    gnome-software
+    gnome-terminal
     gnome-user-docs
     gnome-calculator
     gnome-characters
     gnome-font-viewer
     gnome-text-editor
+    gnome-disk-utility
 
+    orca # screen reader
+    tali # task manager
     yelp # help viewer
     geary # mail client
+    iagno # solitaire
     totem # video player
     baobab # disk usage analyzer
     evince # document viewer
@@ -42,10 +84,6 @@
     };
   };
 
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = vars.userName;
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
@@ -57,13 +95,14 @@
     ]
     ++ (with gnomeExtensions; [
       forge
+      caffeine
+      kimpanel
       open-bar
       appindicator
       blur-my-shell
       app-menu-is-back
+      battery-health-charging
     ]);
-
-  services.udev.packages = with pkgs; [ gnome-settings-daemon ];
 
   programs.dconf.enable = true;
 }
