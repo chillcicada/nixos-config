3860c172684bff201a343e446e3b8b7f34775c18
chillcicada <2210227279@qq.com>
2025-04-18 23:08:00 +0800

    chore: add docker proxy

--- hosts/salt/docker.nix
+++ hosts/salt/docker.nix
@@ -9,6 +9,12 @@
     docker = {
       enable = true;
       autoPrune.enable = true;
+      daemon.settings = {
+        "registry-mirrors" = [
+          "https://docker.1ms.run"
+          "https://docker.xuanyuan.me"
+        ];
+      };
     };
     oci-containers.backend = "docker";
   };
