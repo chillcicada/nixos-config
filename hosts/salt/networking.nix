1172d98b95abf9b0c04fb9bac4cd48a5334540ed
chillcicada <2210227279@qq.com>
2025-04-19 15:25:18 +0800

    chore: update ports

--- hosts/salt/networking.nix
+++ hosts/salt/networking.nix
@@ -9,6 +9,8 @@
         80 # Nginx Services
         81 # Nginx Web Admin
         443 # Nginx Services (HTTPS)
+        7791 # lsky-pro
+        8080 # webdav
       ];
       allowedUDPPorts = [
       ];
