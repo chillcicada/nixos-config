408f066e588df3d068cefbcda0a565bbf6e97846
chillcicada <2210227279@qq.com>
2025-04-30 16:59:59 +0800

    chore: hyprland is hard

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -1,18 +1,59 @@
-{ pkgs, ... }:
+{ pkgs, vars, ... }:
 
 {
-  services.xserver.enable = true;
-  services.xserver.xkb.layout = "us";
 
-  services.xserver.displayManager.gdm.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      excludePackages = with pkgs; [ xterm ];
+
+      xkb = {
+        layout = "us";
+      };
+    };
+
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = vars.userName;
+      };
+    };
+  };
 
   programs.hyprland = {
     enable = true;
 
     xwayland.enable = true;
-
+    systemd.enable = true;
     portalPackage = pkgs.xdg-desktop-portal-hyprland;
   };
 
+  settings = {
+    "$mod" = "SUPER";
+    "$terminal" = "kitty";
+    "$menu" = "fuzzel";
+
+    exec-once = [
+      "fcitx5"
+    ];
+  };
+
   programs.dconf.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    hyprshot # screenshot tool
+    hyprlock # lock screen
+    fuzzel
+    wofi
+    waybar
+    hyprpaper # wallpaper manager
+    waypaper
+    wlogout
+    dunst
+    hyprshade
+    hypridle
+    hyprcursor
+    hyprpicker
+  ];
 }
