42f9e10dc32fc8d9c1440b7f7facb5960d7ae4c5
chillcicada <2210227279@qq.com>
2025-04-30 12:14:30 +0800

    chore: update flake

--- hosts/chill/default.nix
+++ hosts/chill/default.nix
@@ -65,7 +65,13 @@
     keyMap = "us";
   };
 
-  nixpkgs.config.permittedInsecurePackages = [ "electron-11.5.0" ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-11.5.0"
+    "clash-verge-rev-2.2.3"
+    "clash-verge-rev-unwrapped-2.2.3"
+    "clash-verge-rev-webui-2.2.3"
+    "clash-verge-rev-service-2.2.3"
+  ];
 
   programs.nix-ld = {
     enable = true;
