ee37d8036c2d6b1d0144cee5dc0b7d80b7b82524
chillcicada <2210227279@qq.com>
2025-05-08 16:21:12 +0800

    chore

--- hosts/salt/services/nginx.nix
+++ hosts/salt/services/nginx.nix
@@ -6,27 +6,20 @@
     enable = true;
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
-    # other Nginx options
-    virtualHosts."webdav.chillcicada.com" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://0.0.0.0:8080";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-        extraConfig =
-          # required when the target is also TLS server with multiple hosts
-          "proxy_ssl_server_name on;";
-      };
-    };
+
     virtualHosts."img.chillcicada.com" = {
       enableACME = true;
       forceSSL = true;
       locations."/" = {
         proxyPass = "http://0.0.0.0:7791";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-        extraConfig =
-          # required when the target is also TLS server with multiple hosts
-          "proxy_ssl_server_name on;";
+      };
+    };
+
+    virtualHosts."webdav.chillcicada.com" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://0.0.0.0:8080";
       };
     };
   };
