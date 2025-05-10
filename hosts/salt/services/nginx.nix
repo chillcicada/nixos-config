e776b277fbbb1427bfd35b0f2fa54b64f7ce072a
chillcicada <2210227279@qq.com>
2025-05-10 17:35:01 +0800

    chore: nginx

--- hosts/salt/services/nginx.nix
+++ hosts/salt/services/nginx.nix
@@ -7,8 +7,19 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
 
-    virtualHosts."webdav.chillcicada.com" = {
+    virtualHosts."chillcicada.com" = {
       enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        return = "200 '<html><body>Hello world!</body></html>'";
+        extraConfig = ''
+          default_type text/html;
+        '';
+      };
+    };
+
+    virtualHosts."webdav.chillcicada.com" = {
+      useACMEHost = "chillcicada.com";
       forceSSL = true;
       locations."/" = {
         proxyPass = "http://127.0.0.1:8080";
@@ -19,5 +30,11 @@
   security.acme = {
     acceptTerms = true;
     defaults.email = vars.userEmail;
+    certs."chillcicada.com" = {
+      domain = "chillcicada.com";
+      extraDomainNames = [ "webdav.chillcicada.com" ];
+    };
   };
+
+  users.users.nginx.extraGroups = [ "acme" ];
 }
