d249fe7f697bb70e6e8f8f44583e6d9071745ada
chillcicada <2210227279@qq.com>
2025-05-11 21:32:59 +0800

    chore: network

--- hosts/chill/networking.nix
+++ hosts/chill/networking.nix
@@ -13,17 +13,6 @@
 
     useDHCP = lib.mkDefault true;
 
-    extraHosts = ''
-      # extra-ip
-
-      # dev-ip
-      127.0.0.1 jise.dev
-
-      # ban-ip
-      127.0.0.1 bilibili.com
-      127.0.0.1 www.bilibili.com
-    '';
-
     firewall = {
       enable = true;
       # Allow HTTP and SSH traffic
