c804bf8481ecf89a501b3431ae10d936c572b8ce
chillcicada <2210227279@qq.com>
2025-10-01 14:58:47 +0800

    chore

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -33,6 +33,9 @@ in
 
       # for nautilus trash
       gvfs.enable = true;
+
+      # enable Flatpak support
+      flatpak.enable = true;
     };
 
     environment.gnome.excludePackages = with pkgs; [
