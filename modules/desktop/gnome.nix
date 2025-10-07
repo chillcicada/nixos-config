0465098ef1e6c6b2071ec9fc4b83f5ecfe73827a
chillcicada <2210227279@qq.com>
2025-10-07 19:48:07 +0800

    faet: update nixpkgs

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -82,10 +82,6 @@ in
       };
     };
 
-    # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-    systemd.services."getty@tty1".enable = false;
-    systemd.services."autovt@tty1".enable = false;
-
     environment.systemPackages = with pkgs.gnomeExtensions; [
       caffeine
       kimpanel
