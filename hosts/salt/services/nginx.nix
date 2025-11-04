f14dddbde2895f037241d9ee16d2da2010a09377
chillcicada <2210227279@qq.com>
2025-11-04 11:17:10 +0800

    feat: nginx config

--- hosts/salt/services/nginx.nix
+++ hosts/salt/services/nginx.nix
@@ -4,49 +4,55 @@
   # https://wiki.nixos.org/wiki/Nginx
   services.nginx = {
     enable = true;
-    recommendedProxySettings = true;
+
     recommendedTlsSettings = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
 
-    virtualHosts."chillcicada.com" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        return = "200 '<html><body>Hello world!</body></html>'";
-        extraConfig = ''
-          default_type text/html;
-        '';
-      };
-    };
+    virtualHosts =
+      let
+        proxy =
+          host:
+          {
+            port,
+            extra ? "",
+          }:
+          {
+            useACMEHost = host;
+            forceSSL = true;
+            locations."/" = {
+              proxyPass = "http://127.0.0.1:" + toString port;
+              extraConfig = extra;
+            };
+          };
+      in
+      {
+        "chillcicada.com" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            return = "200 '<html><body>Hello world!</body></html>'";
+            extraConfig = ''
+              default_type text/html;
+            '';
+          };
+        };
 
-    virtualHosts."uptime.chillcicada.com" = {
-      useACMEHost = "chillcicada.com";
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8080";
-      };
-    };
+        "uptime.chillcicada.com" = proxy "chillcicada.com" {
+          port = 8080;
+        };
 
-    virtualHosts."webdav.chillcicada.com" = {
-      useACMEHost = "chillcicada.com";
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8081";
-        extraConfig = ''
-          client_max_body_size 1024M;
-        '';
-      };
-    };
+        "webdav.chillcicada.com" = proxy "chillcicada.com" {
+          port = 8081;
+          extra = "client_max_body_size 1024M;";
+        };
 
-    virtualHosts."pastebin.chillcicada.com" = {
-      useACMEHost = "chillcicada.com";
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8082";
-        extraConfig = ''
-          client_max_body_size 1024M;
-        '';
+        "pastebin.chillcicada.com" = proxy "chillcicada.com" {
+          port = 8082;
+          extra = "client_max_body_size 1024M;";
+        };
       };
-    };
   };
 
   security.acme = {
