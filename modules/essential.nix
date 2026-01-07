d6c6886d78d03c6e4303d1ec6269594fc893e7df
chillcicada <2210227279@qq.com>
2026-01-07 23:27:44 +0800

    chore: update substituters

--- modules/essential.nix
+++ modules/essential.nix
@@ -24,14 +24,16 @@ in
       settings = {
         nix-path = lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
         substituters = [
-          "https://cache.nixos.org"
-
+          "https://nixpkgs-update-cache.nix-community.org"
           "https://nix-community.cachix.org"
           "https://chillcicada.cachix.org"
+          "https://cache.nixos.org"
         ];
         trusted-public-keys = [
+          "nixpkgs-update-cache.nix-community.org-1:U8d6wiQecHUPJFSqHN9GSSmNkmdiFW7GW7WNAnHW0SM="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "chillcicada.cachix.org-1:nW+OhwrpD0z5oGcBFt/aLEtzN94XcJBm81/cAHsEwVU="
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         ];
       };
     };
