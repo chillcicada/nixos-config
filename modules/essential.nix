3262ce6d56eff07a7f77831b7f2a0c4e2f633068
chillcicada <2210227279@qq.com>
2026-02-02 08:46:38 +0800

    chore: nix conf

--- modules/essential.nix
+++ modules/essential.nix
@@ -21,12 +21,10 @@ in
 
     nix.settings = {
       substituters = [
-        "https://nixpkgs-update-cache.nix-community.org"
         "https://nix-community.cachix.org"
         "https://chillcicada.cachix.org"
       ];
       trusted-public-keys = [
-        "nixpkgs-update-cache.nix-community.org-1:U8d6wiQecHUPJFSqHN9GSSmNkmdiFW7GW7WNAnHW0SM="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "chillcicada.cachix.org-1:nW+OhwrpD0z5oGcBFt/aLEtzN94XcJBm81/cAHsEwVU="
       ];
