2a412465fba8c667febaaa1f6eb12b25b2c9e61d
chillcicada <2210227279@qq.com>
2026-02-22 20:58:04 +0800

    chore: update flake

--- home/default.nix
+++ home/default.nix
@@ -12,15 +12,5 @@
   ];
 
   # Nixpkgs overlays
-  nixpkgs.overlays = [
-    inputs.chillcicada.overlays.default
-    (_: super: {
-      wechat = super.wechat.overrideAttrs (_: {
-        src = super.fetchurl {
-          url = "https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.AppImage";
-          hash = "sha256-+r5Ebu40GVGG2m2lmCFQ/JkiDsN/u7XEtnLrB98602w=";
-        };
-      });
-    })
-  ];
+  nixpkgs.overlays = [ inputs.chillcicada.overlays.default ];
 }
